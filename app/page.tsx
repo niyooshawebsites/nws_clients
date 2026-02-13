870108bc3f4f8fed33558c9e3d05d688cb5e30db
niyooshawebsites <niyooshawebsites@gmail.com>
2026-02-13 23:00:24 +0530

    turbo pack issues v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,68 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Field, FieldGroup, FieldDescription, FieldLabel } from "@/components/ui/field"
+import {
+  Field,
+  FieldGroup,
+  FieldDescription,
+  FieldLabel,
+} from "@/components/ui/field";
+
+import { useActionState } from "react";
+import { signup } from "@/app/actions/createUser";
+
+const initialState = {
+  error: undefined,
+  success: undefined,
+};
 
 export default function Home() {
+  const [state, formAction] = useActionState(signup, initialState);
+
   return (
     <>
       <h1 className="text-center text-3xl">NWS CRM</h1>
-      <form className="max-w-md mx-auto p-4">
+      <form className="max-w-md mx-auto p-4" action={formAction}>
         <FieldGroup className="border p-5 rounded-md">
+          <Field>
+            <FieldLabel htmlFor="email">Name</FieldLabel>
+            <FieldDescription>Enter your name.</FieldDescription>
+            <Input type="text" name="name" id="name" placeholder="John Doe" />
+          </Field>
           <Field>
             <FieldLabel htmlFor="email">Email</FieldLabel>
             <FieldDescription>Enter your email.</FieldDescription>
-            <Input type="email" id="email" placeholder="john@example.com" />
+            <Input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="john@example.com"
+            />
           </Field>
 
           <Field>
             <FieldLabel htmlFor="password">Password</FieldLabel>
             <FieldDescription>Enter your password</FieldDescription>
-            <Input type="password" id="password" placeholder="***********" />
+            <Input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="***********"
+            />
           </Field>
 
-          <Button type="submit" variant="secondary" className="bg-gray-900 text-gray-100 hover:bg-gray-950 hover:cursor-pointer">Sign in</Button>
+          <Button
+            type="submit"
+            variant="secondary"
+            className="bg-gray-900 text-gray-100 hover:bg-gray-950 hover:cursor-pointer"
+          >
+            Sign up
+          </Button>
         </FieldGroup>
+        {state.error && <p className="text-red-500">{state.error}</p>}
+        {state.success && <p className="text-green-500">{state.success}</p>}
       </form>
     </>
-
   );
 }
---
+++ lib/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "./generated/prisma/client";
+import { PrismaPostgresAdapter } from "@prisma/adapter-ppg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPostgresAdapter({
+  connectionString,
+});
+
+const prismaClientSingleton = () => {
+  return new PrismaClient({ adapter });
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
